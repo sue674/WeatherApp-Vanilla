d0c319adbc8cd69e0d77302106a4374416bfcc99
Sue <gusu674@mahalo-mail.com>
2024-04-30 13:16:17 +0200

    include time and wether details from API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 function refreshWeather(response){
     let temperature = response.data.temperature.current
     let temperatureElement = document.querySelector("#temperature-value");
-    temperatureElement.innerHTML = Math.round(temperature);
-
     let cityElement = document.querySelector("#city");
+    let weatherElement = document.querySelector("#addedInfo");
+    let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#time");
+let timestamp = response.data.time;
+let now = new Date(timestamp * 1000);
+
     cityElement.innerHTML = response.data.city;
+
+
+timeElement.innerHTML= formatDate(now);
+    temperatureElement.innerHTML = Math.round(temperature);
+    weatherElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+windElement.innerHTML = `${response.data.wind.speed} km/h`;
+ 
 }
 
+function formatDate (now){
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] 
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (minutes <10){
+minutes = `0${minutes}`;
+}
+
+return `${day}, ${hours}:${minutes}`
+}
 
 function searchCity (city){
     //make an API call and update user interface
@@ -25,3 +49,8 @@ function handleSearchSubmit (event){
 
 let searchFormElement = document.querySelector("#search-form")
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris")
+
+
+
