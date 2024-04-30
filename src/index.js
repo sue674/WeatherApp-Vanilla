f762eb8eac73a169fd2e0a9ee2bb8147f2e64e0c
Sue <gusu674@mahalo-mail.com>
2024-04-30 12:04:06 +0200

    search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+function refreshWeather(response){
+    let temperature = response.data.temperature.current
+    let temperatureElement = document.querySelector("#temperature-value");
+    temperatureElement.innerHTML = Math.round(temperature);
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+}
+
+
+function searchCity (city){
+    //make an API call and update user interface
+    let apiKey = "feaeta6473b4b23fd60370ob33dc4c40";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit (event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input")
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    
+    searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form")
