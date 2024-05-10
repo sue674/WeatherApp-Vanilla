17616efa351671efd548bacc1d6c551de88e6b3e
Sue <gusu674@mahalo-mail.com>
2024-05-10 10:11:01 +0200

    updated forecast JS

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ function displayForecast(response){
 let forecastHtml = "";
 
 response.data.daily.forEach(function (day, index){
-  if (index <0 && index >6){
+  if (index > 0 && index < 6){
  forecastHtml = forecastHtml + ` 
  <div class = "col2">
               <div class="weather-forecast-date">
